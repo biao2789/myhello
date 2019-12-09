adf0b68f4152300dac9971e4fc9b0fa51ba23798
biao2789 <357390624@qq.com>
2019-12-09 10:42:11 +0800

    add the js and css,almost entry

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -9,7 +9,19 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 bootstrap = Bootstrap(app)
+
 db = SQLAlchemy(app)
 # db.create_all()
+
+
+def register_template_context(app):
+    @app.context_processor
+    def make_template_context():
+        number = 1
+        admin = "nihao"
+        return dict(number=number, admin=admin)
+
+
+ab = register_template_context(app)
 
 from sayhello import forms, views
